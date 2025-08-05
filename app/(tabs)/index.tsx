5c1387b94929abc2265267aa61c7b6057a7c63d5
RutvikCodez <rutvikdarji21@gnu.ac.in>
2025-08-05 14:15:50 +0530

    Integrate Convex for todos management; implement schema, queries, and mutations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,22 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
+  const addTodo = useMutation(api.todos.addTodo);
+  
   return (
     <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle the mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "New Todo" })}>
+        <Text>Add a new todo</Text>
       </TouchableOpacity>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
+import { ConvexReactClient, ConvexProvider } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
+   <ConvexProvider client={convex}>
+     <ThemeProvider>
       <Stack
         screenOptions={{
           headerShown: false,
@@ -12,5 +18,6 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" />
       </Stack>
     </ThemeProvider>
+   </ConvexProvider>
   );
 }
